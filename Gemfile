f750320e42e59162664f6d1ecfe4598d7926c069
Wilfison <wilfisonbatista@gmail.com>
2025-04-14 21:44:59 -0300

    feat: add lint server tests and configuration

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ source 'https://rubygems.org'
 gem 'haml'
 gem 'haml_lint'
 gem 'html2haml'
+gem 'rubocop'
+
+group :development, :test do
+  gem 'minitest'
+  gem 'minitest-reporters'
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,13 @@
+require 'bundler/setup'
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+require 'haml_lint'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+TEST_ROOT_PATH = File.expand_path('..', __dir__)
+HAML_LINT_CONFIG_PATH = File.expand_path('support/.haml-lint.yml', __dir__)
+
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
