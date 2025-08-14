9b79061d7d9b72afa7fbda2c1bc274401448edac
Wilfison <wilfisonbatista@gmail.com>
2025-08-14 11:37:39 -0300

    refactor: harmonize string literal styles and improve formatting in configuration and test files

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
-source 'https://rubygems.org'
+# frozen_string_literal: true
 
-gem 'haml'
-gem 'haml_lint'
-gem 'html2haml'
-gem 'rubocop'
+source "https://rubygems.org"
+
+gem "haml"
+gem "haml_lint"
+gem "html2haml"
+gem "rubocop"
 
 group :development, :test do
-  gem 'minitest'
-  gem 'minitest-reporters'
+  gem "minitest"
+  gem "minitest-reporters"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,15 @@
-require 'bundler/setup'
+# frozen_string_literal: true
 
-require 'minitest/autorun'
-require 'minitest/reporters'
+require "bundler/setup"
 
-require 'haml_lint'
+require "minitest/autorun"
+require "minitest/reporters"
+
+require "haml_lint"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-TEST_ROOT_PATH = File.expand_path('..', __dir__)
-HAML_LINT_CONFIG_PATH = File.expand_path('support/.haml-lint.yml', __dir__)
+TEST_ROOT_PATH = File.expand_path("..", __dir__)
+HAML_LINT_CONFIG_PATH = File.expand_path("support/.haml-lint.yml", __dir__)
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
